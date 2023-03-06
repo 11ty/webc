574334a660f55dd1c703874ca59652019873076b
Zach Leatherman <zachleatherman@gmail.com>
2023-03-06 08:54:10 -0600

    Tests for flow control on asset elements

--- test/flowcontrolTest.js
+++ test/flowcontrolTest.js
@@ -135,3 +135,63 @@ test("Using webc:if with a promise (resolves false)", async t => {
 </body>
 </html>`);
 });
+
+test("Using webc:if=false on a style", async t => {
+	let component = new WebC();
+
+	component.setBundlerMode(true);
+	component.setContent(`<style webc:if="false">* { color: red }</style>Test`);
+
+	let { html, css, js, components } = await component.compile();
+
+	t.deepEqual(js, []);
+	t.deepEqual(css, []);
+	t.deepEqual(components, []);
+
+	t.is(html, `Test`);
+});
+
+test("Using webc:if=true on a style", async t => {
+	let component = new WebC();
+
+	component.setBundlerMode(true);
+	component.setContent(`<style webc:if="true">* { color: red }</style>Test`);
+
+	let { html, css, js, components } = await component.compile();
+
+	t.deepEqual(js, []);
+	t.deepEqual(css, [`* { color: red }`]);
+	t.deepEqual(components, []);
+
+	t.is(html, `Test`);
+});
+
+test("Using webc:if on a style based on host component attribute (true)", async t => {
+	let component = new WebC();
+
+	component.setBundlerMode(true);
+	component.setContent(`<if-component-style :@attr-value="true"></if-component-style>`);
+	component.defineComponents("./test/stubs/if-component-style.webc");
+
+	let { html, css, js, components } = await component.compile();
+
+	t.deepEqual(js, []);
+	t.deepEqual(css, [`* { color: red }`]);
+
+	t.is(html, `<if-component-style>Test</if-component-style>`);
+});
+
+test("Using webc:if on a style based on host component attribute (false)", async t => {
+	let component = new WebC();
+
+	component.setBundlerMode(true);
+	component.setContent(`<if-component-style :@attr-value="false"></if-component-style>`);
+	component.defineComponents("./test/stubs/if-component-style.webc");
+
+	let { html, css, js, components } = await component.compile();
+
+	t.deepEqual(js, []);
+	t.deepEqual(css, []);
+
+	t.is(html, `<if-component-style>Test</if-component-style>`);
+});
