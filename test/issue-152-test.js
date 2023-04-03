9e18b959102c8d4d355ed8a36fb1eca629c6992e
Zach Leatherman <zachleatherman@gmail.com>
2023-04-03 12:05:46 -0500

    Test

--- test/issue-152-test.js
+++ test/issue-152-test.js
@@ -5,8 +5,8 @@ test("Slotted global data access #152", async t => {
 	let component = new WebC();
 
 	component.setBundlerMode(true);
-	component.setInputPath("./test/stubs/issue-152/page.webc");
-	component.defineComponents("./test/stubs/issue-152/site-card.webc");
+	component.setContent(`<parent>Hello I am in the default slot.<span @text="globalData"></span></parent>`);
+	component.defineComponents("./test/stubs/issue-152/parent.webc");
 
 	let { html } = await component.compile({
 		data: {
@@ -14,5 +14,23 @@ test("Slotted global data access #152", async t => {
 		}
 	});
 
-	t.is(html.trim(), `<site-card>Hello I am in the default slot.<span>Hello</span></site-card>`);
+	t.is(html.trim(), `<h1>Hello I am in the default slot.<span>Hello</span></h1>`);
+});
+
+test("Slotted global data access nested #152", async t => {
+	let component = new WebC();
+
+	component.setBundlerMode(true);
+	// component.setContent(`<child>test</child>`);
+	component.setContent(`<child><parent><span @text="globalData"></span></parent></child>`);
+	component.defineComponents("./test/stubs/issue-152/parent.webc");
+	component.defineComponents("./test/stubs/issue-152/child.webc");
+
+	let { html } = await component.compile({
+		data: {
+			globalData: "Hello"
+		}
+	});
+
+	t.is(html.trim(), `<h2><h1><span>Hello</span></h1></h2>`);
 });
