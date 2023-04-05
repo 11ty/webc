88c8ac8634f9e56f35ffc37c2bf39b55664401ec
Leonardo Silveira <sombriks@gmail.com>
2023-04-04 22:02:46 -0300

    more explorative tests for webc:for

---
+++ test/stubs/looping/complex/data.js
@@ -0,0 +1,32 @@
+export const contacts = [
+	{
+		"name":"Joey",
+		"color":"blue",
+		"numbers":[1,2,3]
+	},
+	{
+		"name":"Phoebe",
+		"color":"pink",
+		"numbers":[1,2,3]
+	},
+	{
+		"name":"Chandler",
+		"color":"orange",
+		"numbers":[1,2,3]
+	},
+	{
+		"name":"Rachel",
+		"color":"violet",
+		"numbers":[1,2,3]
+	},
+	{
+		"name":"Monica",
+		"color":"green",
+		"numbers":[1,2,3]
+	},
+	{
+		"name":"Ross",
+		"color":"red",
+		"numbers":[1,2,3]
+	}
+]
