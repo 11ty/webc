23cd02e5b2606cf27cf722e591051c2a9f750f1b
Zach Leatherman <zachleat@users.noreply.github.com>
2026-09-16 16:19:24 -0500

    Fix tests for Eleventy Image v7

--- test/parserTest.js
+++ test/parserTest.js
@@ -1553,7 +1553,7 @@ test("Scripted render function with a require", async t => {
 	]);
 
 	t.is(html, `<div test2="2"></div>
-<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp 300w"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
+<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
 });
 
 test("Scripted render function with an import", async t => {
@@ -1566,7 +1566,7 @@ test("Scripted render function with an import", async t => {
 	]);
 
 	t.is(html, `<div test2="2"></div>
-<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp 300w"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
+<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
 
 });
 
@@ -1582,7 +1582,7 @@ test("Scripted js function with an import", async t => {
 	]);
 
 	t.is(html, `<div test2="2"></div>
-<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp 300w"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
+<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
 
 });
 
@@ -1597,7 +1597,7 @@ test("Scripted webc:setup function with an import #225", async t => {
 		"./test/stubs/render-setup-import.webc",
 	]);
 
-	t.is(html.trim(), `<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp 300w"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
+	t.is(html.trim(), `<picture><source type="image/webp" srcset="/img/6dfd7ac6-300.webp"><img alt="Hi" src="/img/6dfd7ac6-300.jpeg" width="300" height="300"></picture>`);
 
 });
 
